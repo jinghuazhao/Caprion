279be238f55664127b231f9b67d6419c2873ea3d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-11-22 14:43:44 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -1,8 +1,8 @@
-# 21-11-2019 JHZ
+# 22-11-2019 JHZ
 
 git add README.md
 git commit -m "README"
-git add caprion.inc caprion.R makeRLEboxplot.R affymetrix.sh ps.R
+git add caprion.inc caprion.R makeRLEboxplot.R affymetrix.sh bar.R ps.R
 git commit -m "Code"
 git add st.sh
 git commit -m "st.sh"

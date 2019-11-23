aef946644c88819a8e052c54aec3df3268410068
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2019-11-23 05:58:04 +0000

    Code

--- caprion.R
+++ caprion.R
@@ -1,7 +1,7 @@
-# 22-11-2019 JHZ
+# 23-11-2019 JHZ
 
 source("caprion.inc")
-ae(pheno_protein[,-(1:9)],hidden.layers=c(987,30,987))
+ae(pheno_protein[,-(1:9)],hidden.layers=c(987,200,987))
 # ae_caprion(pheno_protein[,-(1:9)])
 
 # UMAP

442b00d80e6a658ca840ef67bec4328e6256c63b
jinghuazhao <jinghuazhao@hotmail.com>
2019-12-10 12:41:43 +0000

    st.sh

--- st.sh
+++ st.sh
@@ -2,10 +2,10 @@
 
 git add README.md
 git commit -m "README"
-git add caprion.ini caprion.R makeRLEboxplot.R affymetrix.sh bar.R ps.R
-git commit -m "Code"
-git add pca_ae_test.*
-git commit -m "PCA-autoencoder comparion"
+git add caprion.ini caprion.R
+git commit -m "Primary Code"
+git add utils
+git commit -m "utilities"
 git add st.sh
 git commit -m "st.sh"
 git push

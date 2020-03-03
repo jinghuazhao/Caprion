76f8059c65bfb5123fadefb01e44c440f5617d5d
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-03 16:35:58 +0000

    utilities

---
+++ utils/qqman.sh
@@ -0,0 +1,17 @@
+# 3-3-2020 JHZ
+
+gunzip -c plink2/P04004-plink2.gz | cut -f1,2,12 --output-delimiter=' ' > VTN.txt
+
+R --slave --vanilla --args \
+  input_data_path=VTN.txt \
+  output_data_rootname=VTN_qq \
+  plot_title="VTN example" < turboqq.r
+
+R --slave --vanilla --args \
+  input_data_path=VTN.txt \
+  output_data_rootname=VTN_man \
+  custom_peak_annotation_file_path=annotate.txt \
+  reference_file_path=turboman_hg19_reference_data.rda \
+  pvalue_sign=5e-8 \
+  plot_title="VTN example" < turboman.r
+

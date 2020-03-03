57818511ed346efb62f2803ad3f23f7cad9c52a0
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-03 11:04:37 +0000

    utilities

--- utils/plink2.sh
+++ utils/plink2.sh
@@ -4,8 +4,6 @@ export TMPDIR=/rds/user/$USER/hpc-work/
 
 module load plink/2.00-alpha
 
-plink --bfile caprion.bgen --maf 0.01 --make-bed --out caprion.01
-awk '{$1=$2};1' caprion.01.fam > caprion.fam
 seq 987 | \
 parallel -C' ' '
   export col=$(cut -d" " -f {} caprion.uniprot); \
--- utils/qctool.sh
+++ utils/qctool.sh
@@ -1,8 +1,8 @@
-# 2-3-2020 JHZ
+# 3-3-2020 JHZ
 
 qctool -g caprion-#.bgen -s SomaLogic.sample -ofiletype binary_ped -og caprion.bgen
 plink --bfile caprion.bgen --maf 0.01 --make-bed --out caprion.01
+awk '{$1=$2};1' caprion.01.fam > caprion.fam
 cut -f2 caprion.01.bim > caprion.01.snpids
-
 qctool -g caprion-#.bgen -s SomaLogic.sample -og caprion.01.bgen -bgen-bits 8 -incl-snpids caprion.01.snpids
 bgenix -g caprion.01.bgen -index -clobber

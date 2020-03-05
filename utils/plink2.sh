6be4ce8f1381f0c0f8c161ff9127ee96dc6265db
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-05 10:52:56 +0000

    utilities

--- utils/plink2.sh
+++ utils/plink2.sh
@@ -4,6 +4,8 @@ export TMPDIR=/rds/user/$USER/hpc-work/
 
 module load plink/2.00-alpha
 
+function binary_ped()
+{
 seq 987 | \
 parallel -C' ' '
   export col=$(cut -d" " -f {} caprion.uniprot); \
@@ -20,3 +22,26 @@ parallel -C' ' '
       rm work/${v}.${v}.glm.linear
   done
 '
+}
+
+function bgen()
+{
+seq 987 | \
+parallel -C' ' '
+  export col=$(cut -d" " -f {} caprion.uniprot); \
+  for v in ${col} ${col}_invn;
+  do
+      echo ${v}
+      plink2 \
+             --bgen caprion.01.bgen --sample caprion.sample \
+             --glm hide-covar --input-missing-phenotype -9 --covar-variance-standardize \
+             --pheno caprion.pheno --pheno-name ${v} --covar caprion.covar \
+             --out work/${v}
+      grep -v NA work/${v}.${v}.glm.linear | \
+      gzip -f > bgen/${v}-plink2.gz
+      rm work/${v}.${v}.glm.linear
+  done
+'
+}
+
+bgen

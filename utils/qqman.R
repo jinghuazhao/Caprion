b228f6a0c5fe555ae0c0682f008b109cc05bd5b7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-01-24 19:33:49 +0000

    utilities

--- utils/qqman.R
+++ utils/qqman.R
@@ -1,4 +1,4 @@
-# 4-3-2020 JHZ
+# 24-1-2021 JHZ
 
 uniprot <- Sys.getenv("uniprot");
 print(uniprot);
@@ -11,11 +11,11 @@ tbl <- within(tbl,{
    BP <- POS
 })
 tbl <- subset(tbl,!is.na(CHR)&!is.na(BP)&!is.na(P))
-# qq <- paste0(uniprot,"_qq.png");
-# png(qq,width=12,height=10,units="in",pointsize=4,res=300)
-# qq(with(tbl,P))
-# dev.off()
+qq <- paste0(uniprot,"_qq.png");
+png(qq,width=12,height=10,units="in",pointsize=4,res=300)
+qq(with(tbl,P))
+dev.off()
 manhattan <- paste0(uniprot,"_manhattan.png");
 png(manhattan,width=12,height=10,units="in",pointsize=4,res=300)
-manhattan(tbl,main=uniprot,genomewideline=-log10(8.210181e-12),suggestiveline=FALSE,ylim=c(0,25));
+manhattan(tbl,main=uniprot,genomewideline=-log10(8.210181e-12),suggestiveline=FALSE,ylim=c(0,15));
 dev.off();

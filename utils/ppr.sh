d950152e603649eb3c72969f0e0bb962a0d81236
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-03-26 22:09:02 +0000

    utilities

--- utils/ppr.sh
+++ utils/ppr.sh
@@ -2,3 +2,7 @@
 
 Rscript -e "knitr::knit('ppr.Rmd')"
 pandoc ppr.md -o ppr.docx
+
+# Rscript -e 'rmarkdown::render("ppr.Rmd", output_format = "html_document")'
+# Rscript -e 'rmarkdown::render("ppr.Rmd", output_format = "pdf_document")'
+# Rscript -e 'rmarkdown::render("ppr.Rmd", output_format = "word_document")'

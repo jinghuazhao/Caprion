834df1b92457194ea975c695a9be17ec7924db48
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-08-09 09:04:06 +0100

    docs

--- docs/utils/gwas2.do
+++ docs/utils/gwas2.do
@@ -7,6 +7,6 @@ insheet using "`caprion'/data2/phase2.dat", delim(" ") case clear
 rename ID_1 id
 format id %15.0g
 merge 1:1 id using "`dir'/data/interval/interval_data"
-gwas2 `trait', studyname(interval) dirgenefiles("`dir'/data/interval") /*
+gwas2 `trait', studyname(interval) dirgenefiles("`caprion'/data2") /*
 */    covariates(agePulse bmi PC1 PC2 PC3 PC4 PC5 PC6 PC7 PC8 PC9 PC10 PC11 PC12 PC13 PC14 PC15 PC16 PC17 PC18 PC19 PC20) /*
 */    class(sexPulse ethnicity classification) dirwork("`caprion'/`y'") outfmt(dta)
--- docs/utils/gwas2.sh
+++ docs/utils/gwas2.sh
@@ -7,10 +7,32 @@ export caprion=${dir}/pilot
 . /etc/profile.d/modules.sh 
 module load ceuadmin/stata
 
+function setup()
+{
+  sed '2,3d' ${caprion}/data2/phase2.sample > ${caprion}/data2/phase2.dat
+  cd data2
+  for chr in {1..22}
+  do
+    ln -sf caprion-${chr}.bgen chr${chr}.bgen
+#   cp caprion-${chr}.bgen.bgi chr${chr}.bgen.bgi
+    ln -sf ${HPC_WORK}/data/interval/chr${chr}.bgen.bgi
+  done
+  ln -sf ${HPC_WORK}/data/interval/SNPinfo.dta.gz
+  ln -sf ${HPC_WORK}/data/interval/Chunks_15.dta
+  cut -d' ' -f1-3 phase2.sample > interval.sample
+  sed '1,2d' interval.sample  > sample_info.txt
+  stata <<\ \ END
+    insheet id idorder missing using sample_info.txt, delim(" ")
+    format id %15.0g
+    format idorder %15.0g
+    gzsave sample_info, replace
+  END
+  rm sample_info.txt
+  cd -
+}
+
 export full=(RCN3_442625488_VADQDGDSMATR RCN3_442666668_EVAKEFDQLTPEESQAR RCN3_All RCN3_DR)
 export abbrev=(RCN3_44262548~n RCN3_44266666~R RCN3_All_invn RCN3_DR_invn)
-
-sed '2,3d' ${caprion}/data2/phase2.sample > ${caprion}/data2/phase2.dat
 
 for i in {0..3} 
 do

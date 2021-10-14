9230920dea665152839e36b3d90ba9894f2f0cb3
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-10-14 11:18:51 +0000

    Deployed 9e82983 with MkDocs version: 1.1.2

--- utils/2021.sh
+++ utils/2021.sh
@@ -5,6 +5,8 @@ export caprion=${dir}/pilot
 if [ ! -d ${caprion}/data3 ]; then mkdir ${caprion}/data3; fi
 if [ ! -d ${caprion}/bgen3 ]; then mkdir ${caprion}/bgen3; fi
 
+R --no-save -q < ${caprion}/utils/eSet.R
+
 R --no-save -q < ${caprion}/utils/2021.R
 
 pandoc 2021.md -o overlap.docx

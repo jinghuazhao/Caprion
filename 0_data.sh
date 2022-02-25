c4151ae93eea4f49d30119c4eee0b89b8b8fa093
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-02-25 17:31:04 +0000

    Analysis

---
+++ 0_data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+export caprion=${HOME}/Caprion/pilot
+export TMPDIR=/rds/user/$USER/hpc-work/
+
+# qctool to combine bgen, extract ids
+# PCA projection
+# fine-mapping
+
+# 196, 1488, 807 ==> 2491
+# [data|data2|data3]/caprion.fam, caprion.bgen.bed/bim/fam, caprion.01.bgen/bgen.bgi
+# [data|data2]/interval.sample
+
+# ${caprion}/data/caprion.sample
+# ${caprion}/data/phase2.sample
+# ${caprion}/data3/protein.sample
+

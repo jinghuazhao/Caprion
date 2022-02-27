8cc58bbe3741f5a7bede9d0a4e8276a8a4ed5b22
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-02-27 17:36:13 +0000

    Analysis

--- 0_data.sh
+++ 0_data.sh
@@ -4,7 +4,7 @@ export autosomes=~/rds/post_qc_data/interval/imputed/uk10k_1000g_b37/
 export ref=~/rds/post_qc_data/interval/reference_files/genetic/reference_files_genotyped_imputed/
 export caprion=${HOME}/Caprion/pilot
 export TMPDIR=${HPC_WORK}/work
-export dir=~/Caprion/pilot/work
+export work=${caprion}/work
 export HGI=~/COVID-19/HGI
 export PCA_projection=${HGI}/pca_projection
 export PCA_loadings=${PCA_projection}/hgdp_tgp_pca_covid19hgi_snps_loadings.rsid.plink.tsv
@@ -13,26 +13,32 @@ export sscore=${PCA_projection}/hgdp_tgp_pca_covid19hgi_snps_scores.txt.gz
 
 module load plink/2.00-alpha ceuadmin/stata
 
-cut -f1 ${PCA_loadings} | tail -n +2 > ${dir}/variants.extract
-bgenix -g ${caprion}/data/caprion.01.bgen -incl-rsids ${dir}/variants.extract > ${dir}/caprion.bgen
-bgenix -g ${dir}/caprion.bgen -index -clobber
+cut -f1 ${PCA_loadings} | tail -n +2 > ${work}/variants.extract
 
-sqlite3 ${dir}/caprion.bgen.bgi <<END
+function pca_project()
+# PCA projection
+{
+export dir=${1}
+export phase=${2}
+bgenix -g ${caprion}/${dir}/caprion.01.bgen -incl-rsids ${work}/variants.extract > ${work}/${phase}.bgen
+bgenix -g ${work}/${phase}.bgen -index -clobber
+
+sqlite3 ${work}/${phase}.bgen.bgi <<END
 .tables
 .separator "\t"
 .header on
-.output ${dir}/metadata.txt
+.output ${work}/metadata.txt
 select * from metadata;
-.output ${dir}/Variant.txt
+.output ${work}/Variant.txt
 select * from Variant;
 END
 
-plink2 --bgen ${dir}/caprion.bgen ref-first --make-pfile --out ${dir}/caprion
+plink2 --bgen ${work}/${phase}.bgen ref-first --make-pfile --out ${work}/${phase}
 
 set -eu
 
 plink2 \
-    --pfile ${dir}/caprion \
+    --pfile ${work}/${phase} \
     --score ${PCA_loadings} \
             variance-standardize \
             cols=-scoreavgs,+scoresums \
@@ -40,40 +46,44 @@ plink2 \
             header-read \
     --score-col-nums 3-12 \
     --read-freq ${PCA_af} \
-    --out ${dir}/caprion
+    --out ${work}/${phase}
 
 awk '
   {
     if (NR==1) $1="#FID IID"
     else $1=0" "$1
     print
-  }' ${dir}/caprion.sscore > ${dir}/rsid.sscore
-ln -sf ${dir}/caprion.sscore.vars ${dir}/rsid.sscore.vars
+  }' ${work}/${phase}.sscore > ${work}/${phase}-rsid.sscore
+ln -sf ${work}/${phase}.sscore.vars ${work}/${phase}-rsid.sscore.vars
 
 cat <(echo FID IID dummy | tr ' ' '\t') \
-    <(sed '1d' ${dir}/caprion.psam | awk -v OFS='\t' '{print 0,$1,0}') > ${dir}/rsid.pheno
+    <(sed '1d' ${work}/${phase}.psam | awk -v OFS='\t' '{print 0,$1,0}') > ${work}/${phase}-rsid.pheno
 
 export ethnic_file=${HGI}/ethnic.txt
 cat <(head -1  ${ethnic_file}) \
-    <(cut -f1 ${dir}/caprion.psam | grep -f - ${ethnic_file}) > ${dir}/ethnic.txt
+    <(cut -f1 ${work}/${phase}.psam | grep -f - ${ethnic_file}) > ${work}/${phase}-ethnic.txt
 export covar_file=${HGI}/work/snpid.covars
 cat <(head -1  ${covar_file}) \
-    <(cut -f1 ${dir}/caprion.psam | grep -f - ${covar_file}) > ${dir}/covars.txt
+    <(cut -f1 ${work}/${phase}.psam | grep -f - ${covar_file}) > ${work}/${phase}-covars.txt
 
 Rscript ${PCA_projection}/plot_projected_pc.R \
-        --sscore ${dir}/rsid.sscore \
-        --phenotype-file ${dir}/rsid.pheno \
+        --sscore ${work}/${phase}-rsid.sscore \
+        --phenotype-file ${work}/${phase}-rsid.pheno \
         --phenotype-col dummy \
-        --covariate-file ${dir}/covars.txt \
+        --covariate-file ${work}/${phase}-covars.txt \
         --pc-prefix PC \
         --pc-num 20 \
-        --ancestry-file ${dir}/ethnic.txt \
+        --ancestry-file ${work}/${phase}-ethnic.txt \
         --ancestry-col ethnic \
         --study phase1 \
-        --out ${dir}/caprion
+        --out ${work}/${phase}
+}
 
-# qctool to combine bgen, extract ids
-# PCA projection
+pca_project data phase1
+pca_project data2 phase2
+pca_project data3 phase3
+
+# qctool to combine bgen -- handling MAF<=0.01
 # fine-mapping
 
 # 196, 1488, 807 ==> 2491
@@ -83,4 +93,3 @@ Rscript ${PCA_projection}/plot_projected_pc.R \
 # ${caprion}/data/caprion.sample
 # ${caprion}/data/phase2.sample
 # ${caprion}/data3/protein.sample
-# stata -b do ${HGI}/ethnic.do

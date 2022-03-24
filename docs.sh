fa365c5520497041d83b32a197bab467ca97f78a
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-03-24 14:49:30 +0000

    utils

--- docs.sh
+++ docs.sh
@@ -12,19 +12,19 @@ mkdocs gh-deploy
 
 git add .gitignore
 git commit -m ".gitignore"
-git add README.md
+git add README.md analysis/README.md pilot/README.md
 git commit -m "README"
-git add 1_pca_projection.sh 2_ggm.R 3_wgcna.R 4_pca_clustering.R 5_pgwas.sh 6_meta_analysis.sh
+git add analysis/1_pca_projection.sh analysis/2_ggm.R analysis/3_wgcna.R analysis/4_pca_clustering.R analysis/5_pgwas.sh analysis/6_meta_analysis.sh
 git commit -m "Analysis"
-git add caprion.ini caprion.R caprion.ipynb
+git add pilot/caprion.ini pilot/caprion.R pilot/caprion.ipynb
 git commit -m "Primary Code"
-git add gwas2.md
+git add pilot/gwas2.md
 git commit -m "gwas2"
-git add tests.md
+git add pilot/tests.md
 git commit -m "tests"
-git add utils docs.sh
+git add docs.sh pilot/utils pilot/docs.sh
 git commit -m "utils"
-git add mkdocs.yml
+git add mkdocs.yml pilot/mkdocs.yml
 git commit -m "mkdocs.yml"
 git push
 # git remote set-url origin https://github.com/jinghuazhao/Caprion.git

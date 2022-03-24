230a4f86a55b2689f2d7c06c0000c7156528cbb1
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-03-24 20:06:23 +0000

    .gitignore

--- pilot/utils/ppr.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-Rscript -e "knitr::knit('ppr.Rmd')"
-pandoc ppr.md -o ppr.docx
-
-# Rscript -e 'rmarkdown::render("ppr.Rmd", output_format = "html_document")'
-# Rscript -e 'rmarkdown::render("ppr.Rmd", output_format = "pdf_document")'
-# Rscript -e 'rmarkdown::render("ppr.Rmd", output_format = "word_document")'

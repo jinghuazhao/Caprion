ef78532bca513f2b2098fa1e1a7a4bf62a10e4ab
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-03-24 19:38:07 +0000

    utils

--- pilot/docs.sh
+++ pilot/docs.sh
@@ -1,1 +1,30 @@
-../../pilot/docs.sh
+#!/usr/bin/bash
+
+function setup()
+{
+  module load python/3.7
+  source ~/COVID-19/py37/bin/activate
+}
+
+setup
+mkdocs build
+mkdocs gh-deploy
+
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add 1_pca_projection.sh 2_ggm.R 3_wgcna.R 4_pca_clustering.R 5_pgwas.sh 6_meta_analysis.sh
+git commit -m "Analysis"
+git add caprion.ini caprion.R caprion.ipynb
+git commit -m "Primary Code"
+git add gwas2.md
+git commit -m "gwas2"
+git add tests.md
+git commit -m "tests"
+git add utils docs.sh
+git commit -m "utils"
+git add mkdocs.yml
+git commit -m "mkdocs.yml"
+git push
+# git remote set-url origin https://github.com/jinghuazhao/Caprion.git

622fa060216cb3321406a2f48601552be7d95bfa
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-03-25 08:28:02 +0000

    Deployed b7eac1b with MkDocs version: 1.1.2

--- docs.sh
+++ docs.sh
@@ -4,7 +4,7 @@ function setup()
 {
   module load python/3.7
   source ~/COVID-19/py37/bin/activate
-  for f in README.md autoencoder.md caprion.ini caprion.ipynb caprion.R docs.sh gwas2.md mkdocs.yml utils;do cp -r -p ../pilot/${f} pilot; done
+  for f in .gitignore README.md autoencoder.md caprion.ini caprion.ipynb caprion.R docs.sh gwas2.md mkdocs.yml utils;do cp -r -p ../pilot/${f} pilot; done
 }
 
 setup

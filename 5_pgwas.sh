3cce7364001a516acd0f994ac4bda929e79c0a83
Jing Hua Zhao <jinghuazhao@hotmail.com>
2022-03-30 22:16:12 +0100

    Analysis

--- 5_pgwas.sh
+++ 5_pgwas.sh
@@ -19,16 +19,7 @@ function X()
          --out ~/Caprion/analysis/work/X
   bgenix -g ~/Caprion/analysis/work/X.bgen -index -clobber
 }
-
-function bgen()
-{
-  for chr in chr{1..22}
-  do
-     plink2 --vcf ${caprion}/work/${chr}.vcf.gz --export bgen-1.2 bits=8 --double-id --dosage-erase-threshold 0.001 \
-            --out ${caprion}/work/${chr}
-     bgenix -g ${caprion}/work/${chr}.bgen -index -clobber
-  done
-}
+# bcftools annotate --set-id '%CHROM:%POS\_%REF\/%FIRST_ALT' ${X}/INTERVAL_X_imp_ann_filt_v2.vcf.gz -O z -o work/INTERVAL-X-vcf.gz
 
 function fastGWAsetup()
 {
@@ -43,52 +34,66 @@ function fastGWAsetup()
     join -a1 -e NA -o1.1,1.2,1.3,2.2 - ${caprion}/work/caprion.sex
   ) > ${caprion}/work/caprion.sample
 
-  bcftools query -l ${caprion}/work/chrX.vcf.gz | awk '{print $1,$1}' > ${caprion}/work/-chrX.idlist
+  bcftools query -l ${caprion}/work/chrX.vcf.gz | awk '{print $1,$1}' > ${caprion}/work/chrX.idlist
   bcftools query -f "%ID\n" ${caprion}/work/chrX.vcf.gz > ${caprion}/work/chrX.snplist
 
 # fastGWA mixed model
   sed -i '1d' ${caprion}/work/caprion.pheno
   sed -i '1d' ${caprion}/work/caprion-lr.pheno
+
+  cut -f1,2 --complement ${caprion}/work/${caprion}.pheno | \
+  head -1 | \
+  tr '\t' '\n' > ${caprion}/work/caprion.varlist
 }
 
-if [ ! -f ${caprion}/work/caprion.varlist ]; then
-   cut -f1,2 --complement ${caprion}/work/${caprion}.pheno | head -1 | tr '\t' '\n' > ${caprion}/work/caprion.varlist
-fi
-fastGWAsetup
-sbatch --export=ALL ${caprion}/5_pgwas.sb
+# sbatch --export=ALL ${caprion}/5_pgwas.sb
 
-ls ${caprion}/work/*.fastGWA.gz | \
-xargs -l basename | \
-sed 's/wes-//g;s/wgs-//g;s/.fastGWA.gz//g' | \
-grep -v chrX | \
-grep -v -f - ${caprion}/work/caprion.varlist > ${caprion}/work/caprion.lrlist
+function collect()
+{
+  ls ${caprion}/work/*.fastGWA.gz | \
+  xargs -l basename | \
+  sed 's/wes-//g;s/wgs-//g;s/.fastGWA.gz//g' | \
+  grep -v chrX | \
+  grep -v -f - ${caprion}/work/caprion.varlist > ${caprion}/work/caprion.lrlist
 
-parallel -j10 --env caprion -C' ' '
-  export olink_protein={1}
-  if [ -f ${caprion}/work/{2}-{1}.fastGWA.gz ] && [ -f ${caprion}/work/{2}-{1}-chrX.fastGWA.gz ]; then
-     export out=${olink_protein}.txt.bgz
-     (
-       echo -e "CHR\tSNP\tPOS\tEFF_ALLELE\tOTHER_ALLELE\tN\tEFF_ALLELE_FREQ\tBETA\tSE\tP"
-       gunzip -c ${caprion}/work/spa/{2}-{1}.fastGWA.gz | \
-       sed "1d"
-       gunzip -c ${caprion}/work/spa/{2}-{1}-chrX.fastGWA.gz | \
-       sed "1d"
-     ) | \
-     awk -vOFS="\t" "{print \$2,\$1,\$3,\$6,\$4,\$5,\$7,\$8,\$9,\$10}" | \
-     bgzip -f > ${caprion}/work/${out}
-     tabix -f -S1 -s2 -b3 -e3 {caprion}/work/${out}
-  fi
-' ::: $(cat ${caprion}/work/caprion.varlist)
-# ::: $(grep -f ${caprion}/work/wes.lrlist ${caprion}/work/wes.varlist) ::: wes
+  parallel -j10 --env caprion -C' ' '
+    export olink_protein={1}
+    if [ -f ${caprion}/work/{2}-{1}.fastGWA.gz ] && [ -f ${caprion}/work/{2}-{1}-chrX.fastGWA.gz ]; then
+       export out=${olink_protein}.txt.bgz
+       (
+         echo -e "CHR\tSNP\tPOS\tEFF_ALLELE\tOTHER_ALLELE\tN\tEFF_ALLELE_FREQ\tBETA\tSE\tP"
+         gunzip -c ${caprion}/work/spa/{2}-{1}.fastGWA.gz | \
+         sed "1d"
+         gunzip -c ${caprion}/work/spa/{2}-{1}-chrX.fastGWA.gz | \
+         sed "1d"
+       ) | \
+       awk -vOFS="\t" "{print \$2,\$1,\$3,\$6,\$4,\$5,\$7,\$8,\$9,\$10}" | \
+       bgzip -f > ${caprion}/work/${out}
+       tabix -f -S1 -s2 -b3 -e3 {caprion}/work/${out}
+    fi
+  ' ::: $(cat ${caprion}/work/caprion.varlist)
+}
 
 # Rscript -e 'library(HIBAG)'
-# bcftools annotate --set-id '%CHROM:%POS\_%REF\/%FIRST_ALT' ${X}/INTERVAL_X_imp_ann_filt_v2.vcf.gz -O z -o work/INTERVAL-X-vcf.gz
 
-seq 22 | \
-xargs -I {} cut -f15 ${ref}/impute_{}_interval.snpstats | \
-grep -v MAF | \
-sed '/^$/d' | \
-Rscript -e 'maf <- scan("stdin"); cutoffs <- cut(maf,c(0,0.001,0.01,0.05,0.1,0.2,0.3,0.4));table(cutoffs);table(cutoffs)/length(maf)'
+function bgen()
+{
+  for chr in chr{1..22}
+  do
+     plink2 --vcf ${caprion}/work/${chr}.vcf.gz --export bgen-1.2 bits=8 --double-id --dosage-erase-threshold 0.001 \
+            --out ${caprion}/work/${chr}
+     bgenix -g ${caprion}/work/${chr}.bgen -index -clobber
+  done
+}
+
+function tableMAF()
+{
+  seq 22 | \
+  xargs -I {} cut -f15 ${ref}/impute_{}_interval.snpstats | \
+  grep -v MAF | \
+  sed '/^$/d' | \
+  Rscript -e 'maf <- scan("stdin"); cutoffs <- cut(maf,c(0,0.001,0.01,0.05,0.1,0.2,0.3,0.4));table(cutoffs);table(cutoffs)/length(maf)'
+}
 
 # Read 87696888 items
 # cutoffs

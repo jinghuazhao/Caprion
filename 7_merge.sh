9e2055a397c1520d3ec155404cda09dca26c23f7
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-05-31 17:26:16 +0100

    Analysis

--- 7_merge.sh
+++ 7_merge.sh
@@ -25,8 +25,7 @@ function signals()
   '
 ) > ${caprion}/work/caprion.signals
 
-
-function cistrans()
+function merge()
 {
   cat <(gunzip -c ${caprion}/METAL/*-1.tbl.gz | head -1 | paste <(echo prot) -) \
       <(sed '1d;s/\t/ /g' ${work}/caprion.signals | grep -v 'X:' | \
@@ -36,6 +35,10 @@ function cistrans()
       > ${work}/caprion.merge
   cut -f11,14 work/caprion.merge | sed '1d' | awk -vOFS="\t" '{printf $2" "; if($1<0) print "-"; else print "+"}' | \
   sort -k1,1 -k2,2 | uniq -c | awk -vOFS="\t" '{print $1,$2,$3}'> ${work}/caprion.dir
+}
+
+function cistrans()
+{
   Rscript -e '
   # Directions
     work <- Sys.getenv("work")
@@ -56,35 +59,51 @@ function cistrans()
             select(Gene,chr,start,end)
     X <- with(ucsc,chr=="X")
     ucsc[X,"chr"] <- "23"
-    added_gene <- subset(glist_hg19,gene %in%c("APOC2","APOC4","CZIB","FYB1","SEPTIN2")) %>%
-                  rename(Gene=gene)
-    ucsc <- bind_rows(ucsc,added_gene)
     caprion <- select(pQTLtools::caprion,Protein,Accession,Gene) %>%
                mutate(Protein=gsub("_HUMAN","",Protein)) %>%
                rename(prot=Protein)
+    quadruple <- function(d,label) data.frame(Gene=label,chr=min(d$chr),start=min(d$start),end=max(d$end))
     caprion[c(55,237,433,435),]
     subset(glist_hg19,grepl("^AMY1",gene))
     subset(glist_hg19,grepl("^C4B",gene)&chr=="6")
-
+    subset(glist_hg19,grepl("^HIST1H4|^HIST2H4[A-B]|HIST4H4",gene))
+    subset(glist_hg19,grepl("^HBA",gene))
+    AMY <- quadruple(subset(glist_hg19,grepl("^AMY1",gene)),label="AMY")
+    C4B <- quadruple(subset(glist_hg19,grepl("^C4B",gene)&chr=="6"),label="C4B")
+    HIST <- quadruple(subset(glist_hg19,grepl("^HIST1H4|^HIST2H4[A-B]|HIST4H4",gene)),label="HIST")
+    HBA <- quadruple(subset(glist_hg19,grepl("^HBA",gene)),label="HBA")
+    caprion_modified <- caprion
+    caprion_modified[55,"Gene"] <- "AMY"
+    caprion_modified[237,"Gene"] <- "C4B"
+    caprion_modified[278,"Gene"] <- "C1orf123"
+    caprion_modified[385,"Gene"] <- "FYB"
+    caprion_modified[433,"Gene"] <- "HIST"
+    caprion_modified[435,"Gene"] <- "HBA"
+    caprion_modified[845,"Gene"] <- "SEPT2"
+    APOC <- subset(glist_hg19,gene %in%c("APOC2","APOC4")) %>%
+            rename(Gene=gene)
+    ucsc_modified <- bind_rows(ucsc,APOC,AMY,C4B,HIST,HBA)
     pqtls <- select(merged,prot,SNP,log.P.) %>%
              rename(log10P=log.P.) %>%
-             left_join(caprion) %>%
+             left_join(caprion_modified) %>%
              select(Gene,SNP,log10P)
     posSNP <- select(merged,SNP,Chr,bp)
     suppressMessages(library(iBMQ))
-    cis.vs.trans <- eqtlClassifier(pqtls,posSNP,ucsc,1e6)
+    cis.vs.trans <- eqtlClassifier(pqtls,posSNP,ucsc_modified,1e6)
     table(cis.vs.trans$Type)
   ##eqtlClassifier
     posGene <- select(glist_hg19,gene,chr,start,end)
     cvt <- eqtlClassifier(pqtls,posSNP,posGene,1e6)
     table(cvt$Type)
   ##cis.vs.trans.classification
-    panel <- left_join(caprion,glist_hg19,by=c('Gene'='gene')) %>%
+    panel <- left_join(caprion_modified,ucsc) %>%
              filter(prot %in% unique(merged$prot))
     cistrans <- gap::cis.vs.trans.classification(merged,panel,"prot")
     cvt <- with(cistrans,data)
   '
 }
+
+cistrans
 
 # Number of proteins which contribute pQTLs
 sed '1d' work/caprion.signals |cut -d' ' -f1 | sort -k1,1 | uniq | wc -l

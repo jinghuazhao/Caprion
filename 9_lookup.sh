653703971875cc8ea5a522c09000ce768a79cd43
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-06-13 16:11:29 +0100

    Analysis

--- 9_lookup.sh
+++ 9_lookup.sh
@@ -7,4 +7,7 @@ export phenoname=$(awk 'NR==ENVIRON["SLURM_ARRAY_TASK_ID"]{print $1}' ${caprion}
 export pilot=~/Caprion/pilot
 export work=${analysis}/work
 
+cat ${pilot}/caprion_inf1.chk 
+cut -d',' -f2 ${pilot}/caprion_inf1.chk | grep -f - ${INF}/work/INF1.METAL
 cut -d',' -f2 ${pilot}/caprion_inf1.chk | grep -f - ${INF}/work/INF1.merge
+cut -d, -f7 ${pilot}/caprion_inf1.chk | grep -f - -w ${work}/caprion.merge

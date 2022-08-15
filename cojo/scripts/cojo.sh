436c2a109a4676db527889b9d8d5906465887b23
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-08-15 11:50:08 +0100

    Snakemake profiles

--- cojo/scripts/cojo.sh
+++ cojo/scripts/cojo.sh
@@ -17,43 +17,39 @@ function gcta()
       <(gunzip -c METAL/sentinels/${p}.p.gz | awk '{print $3,toupper($4),toupper($5),$6,$10,$11,10^$12,$18}') > work/${p}.ma
   cut -d' ' -f1 work/${p}.ma | sed '1d' > work/${p}.rsid
   plink2 --bgen data/chr${chr}.bgen ref-last --sample data/caprion.sample \
-         --extract work/${p}.rsid --export ind-major-bed --maf 0.01 --out work/chr${chr}
-  gcta-1.9 --bfile work/chr${chr} \
-           --cojo-file work/${p}.ma \
-           --cojo-slct \
-           --cojo-p 5e-8 \
-           --cojo-collinear 0.9 \
-           --out results/${pr}
-}
-
-gcta
-
-function dosage()
-{
-  plink2 --bfile ${bfile} \
-         --chr ${chr} --from-bp ${start} --to-bp ${end} \
-         --geno 0.1 --mind 0.1 --maf 0.01 --indep-pairwise 1000kb 1 0.1 --out results/${pr}
-  if [ $(grep -w ${r} results/${pr}.prune.in | wc -l) -eq 0 ]; then
-     export i=$(grep -w -f results/${pr}.prune.in {bfile}.bim | \
+         --extract work/${p}.rsid \
+         --geno 0.1 --mind 0.1 --maf 0.01 --indep-pairwise 1000kb 1 0.1 --out results/${pr}.prune
+  if [ $(grep -w ${r} results/${pr}.prune.prune.in | wc -l) -eq 0 ]; then
+     export i=$(grep -w -f results/${pr}.prune.prune.in ${bfile}.bim | \
                 awk -vpos=${pos} 'function abs(x) {if (x<0) return -x; else return x;} {d=abs($4-pos);print $1, $2, $4, d}' | \
                 sort -r -k4,4n | \
                 awk 'NR==1 {print $2}' \
               )
-     sed -i 's/'"$i"'/'"$r"'/g' results/${pr}.prune.in
+     sed -i 's/'"$i"'/'"$r"'/g' results/${pr}.prune.prune.in
   fi
-  if [ ${chr} -eq 19 ]; then
-     sort results/${pr}.prune.in | \
-     join -v1 - ${INF}/work/NLRP2 > results/${pr}.prune
-  else
-     sort results/${pr}.prune.in > results/${pr}.prune
-  fi
-  rm results/${pr}.prune.in results/${pr}.prune.out
-  plink-2 --bgen data/chr${chr}.bgen ref-last \
-          --sample data/caprion.sample \
-          --extract prune/${pr}.prune \
-          --recode A include-alt \
-          --out results/${pr}.dosage
+  (
+    sort results/${pr}.prune.in
+    if [ ${chr} -eq 19 ]; then
+       join -v1 results/${pr}.prune.prune.in ${INF}/work/NLRP2
+    fi
+  ) > results/${pr}.prune
+  rm results/${pr}.prune.prune.in results/${pr}.prune.prune.out work/${p}.rsid
+  plink2 --bgen data/chr${chr}.bgen ref-last --sample data/caprion.sample \
+         --extract results/${pr}.prune --export ind-major-bed --out work/chr${chr}
+  plink2 --bgen data/chr${chr}.bgen ref-last \
+         --sample data/caprion.sample \
+         --extract results/${pr}.prune \
+         --recode A include-alt \
+         --out results/${pr}.dosage
+  gcta-1.9 --bfile work/chr${chr} \
+           --cojo-file work/${p}.ma --extract-region-snp ${r} 1000 --maf 0.01 \
+           --cojo-slct \
+           --cojo-p 5e-8 \
+           --cojo-collinear 0.9 \
+           --out results/${pr}.gcta
 }
+
+gcta
 
 # mkdir data
 # cd data

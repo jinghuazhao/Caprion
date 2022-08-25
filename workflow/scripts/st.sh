daff2ab374a0daf5640cd248c64cec9e4c779d33
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2022-08-25 15:27:36 +0100

    st.sh

--- workflow/scripts/st.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-module load miniconda3/4.5.1
-export csd3path=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop
-source activate ${csd3path}/miniconda37
-
-# snakemake -c --cluster-config slurm/config.yaml -s slurm/rules/cojo.yaml -d slurm

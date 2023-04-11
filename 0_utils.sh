10374f87996b6a5069568c3ad1fe83845308a497
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-04-11 21:29:07 +0100

    Analysis

--- 0_utils.sh
+++ 0_utils.sh
@@ -3,22 +3,90 @@
 export TMPDIR=${HPC_WORK}/work
 export pilot=~/Caprion/pilot
 export analysis=~/Caprion/analysis
-export batch=1
-export SLURM_ARRAY_TASK_ID=14
-export protein=$(awk 'NR==ENVIRON["SLURM_ARRAY_TASK_ID"]{print $1}' ${pilot}/work/caprion.varlist)
 export interval=${HPC_WORK}/data/interval
 
+for SLURM_ARRAY_TASK_ID in 14
+do
+export SLURM_ARRAY_TASK_ID=${SLURM_ARRAY_TASK_ID}
+export protein=$(awk 'NR==ENVIRON["SLURM_ARRAY_TASK_ID"]{print $1}' ${pilot}/work/caprion.varlist)
+if [ ! -d ${analysis}/peptide/${protein} ]; then mkdir ${analysis}/peptide/${protein}; fi
+export sbatch=${analysis}/peptide/${protein}/${protein}.sb
+
 Rscript -e '
- library(Biobase)
- library(dplyr)
- batches <- c("ZWK","ZYQ","UDP")
- batch <- Sys.getenv("batch") %>% as.numeric
- code <- batches[batch]
- protein <- Sys.getenv("protein")
- load(paste0("~/Caprion/pilot/",code,".rda"))
- peptide_ids <- subset(get(paste("mapping",code,sep="_")),Protein==paste0(protein,"_HUMAN"))[["Isotope.Group.ID"]]
- peptide_exprs <- exprs(get(paste("peptide",code,sep="_")))
- peptide_exprs <- subset(peptide_exprs, rownames(peptide_exprs) %in% peptide_ids)
- peptide_dat <- data.frame(id=colnames(peptide_exprs),t(peptide_exprs))
- write.table(peptide_dat,file=paste0("~/",code,".pheno"),col.names=gsub("^[X]","",names(peptide_dat)),row.names=FALSE,quote=FALSE)
+  suppressMessages(library(Biobase))
+  suppressMessages(library(dplyr))
+  analysis <- Sys.getenv("analysis")
+  protein <- Sys.getenv("protein")
+  es_peptide <- function(batch,batches=c("ZWK","ZYQ","UDP"),verbose=FALSE)
+  {
+    code <- batches[batch]
+    load(paste0("~/Caprion/pilot/",code,".rda"))
+    peptide_ids <- subset(get(paste("mapping",code,sep="_")),Protein==paste0(protein,"_HUMAN"))[["Isotope.Group.ID"]]
+    peptide_exprs <- exprs(get(paste("peptide",code,sep="_")))
+    peptide_exprs <- subset(peptide_exprs, rownames(peptide_exprs) %in% peptide_ids)
+    peptide_dat <- data.frame(id=colnames(peptide_exprs),t(peptide_exprs))
+    if (verbose) write.table(peptide_dat,file=paste0(analysis,"/peptide/",protein,"/",code,".pheno"),
+                             col.names=gsub("^[X]","",names(peptide_dat)),row.names=FALSE,quote=FALSE)
+    peptide_dat
+  }
+  ZWK <- es_peptide(1)
+  ZYQ <- es_peptide(2)
+  UDP <- es_peptide(3)
+  caprion <- bind_rows(ZWK,ZYQ,UDP)
+  write.table(caprion,file=paste0(analysis,"/peptide/",protein,"/",protein,".pheno"),
+              col.names=gsub("^[X]","",names(caprion)),row.names=FALSE,quote=FALSE)
 '
+
+(
+echo "#"'!'"/usr/bin/bash"
+echo
+echo "#SBATCH --account CARDIO-SL0-CPU"
+echo "#SBATCH --partition cardio"
+echo "#SBATCH --qos=cardio"
+echo "#SBATCH --mem=28800"
+echo "#SBATCH --time=12:00:00"
+echo "#SBATCH --job-name=${protein}"
+echo "#SBATCH --output=${analysis}/peptide/${protein}/${protein}.o"
+echo "#SBATCH --error=${analysis}/peptide/${protein}/${protein}.e"
+echo
+) > ${sbatch}
+cat << 'EOL' >> ${sbatch}
+function fastLR()
+{
+  for peptide in $(head -1 ${analysis}/peptide/${protein}/${protein}.pheno | cut -d' ' -f1 --complement)
+  do
+  gcta-1.9 --mbgen ${pilot}/work/caprion.bgenlist \
+           --sample ${pilot}/work/caprion.sample \
+           --keep ${pilot}/work/caprion-${batch}.id \
+           --fastGWA-lr \
+           --pheno ${analysis}/peptide/${protein}/${protein}-${batch}.pheno --mpheno ${peptide} \
+           --threads 10 \
+           --out ${analysis}/peptide/${protein}/${protein}-${batch}-${peptide}
+
+  gcta-1.9 --mbgen ${pilot}/work/caprion.bgenlist \
+           --sample ${pilot}/work/caprion.sample \
+           --keep ${pilot}/work/chrX-${batch}.id \
+           --fastGWA-lr --model-only \
+           --pheno ${analysis}/work/caprion-${batch}.pheno --mpheno ${peptide} \
+           --threads 10 \
+           --out ${analysis}/peptide/${protein}/${protein}-${batch}-${peptide}
+
+  gcta-1.9 --bgen ${pilot}/work/chrX.bgen \
+           --sample ${pilot}/work/chrX.sample \
+           --keep ${pilot}/work/chrX-${batch}.id \
+           --load-model ${analysis}/peptide/${protein}/${protein}-${batch}-${peptide}.fastGWA \
+           --extract ${pilot}/work/chrX.snplist --geno 0.1 \
+           --threads 10 \
+           --out ${analysis}/peptide/${protein}/${protein}-${batch}-${peptide}-chrX
+  gzip -f ${analysis}/peptide/${protein}/${protein}-${batch}-${peptide}*fastGWA
+  done
+}
+
+fastLR 1
+fastLR 2
+fastLR 3
+
+EOL
+
+echo sbatch ${sbatch}
+done

43acfe38165ea75b9f334b3f64978d356b5b5358
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2023-04-12 13:51:27 +0100

    Analysis

--- 0_utils.sh
+++ 0_utils.sh
@@ -17,33 +17,49 @@ Rscript -e '
   pilot <- Sys.getenv("pilot")
   analysis <- Sys.getenv("analysis")
   protein <- Sys.getenv("protein")
-  ids <- read.delim(file.path(pilot,"work","caprion.pheno")) %>%
-         select(Affymetrix_gwasQC_bl,caprion_id) %>%
-         mutate(FID=Affymetrix_gwasQC_bl,IID=Affymetrix_gwasQC_bl,id=caprion_id) %>%
-         select(FID,IID,id)
-  es_peptide <- function(batch,batches=c("ZWK","ZYQ","UDP"),verbose=FALSE)
+  pcs <- paste0("ppc",1:3)
+  ccovars <- c("sexPulse","agePulse",pcs,paste0("PC",1:20))
+  dat <- read.delim(file.path(pilot,"work","caprion.pheno"))[c("FID","IID","caprion_id",ccovars)] %>%
+         rename(id=caprion_id)
+  normalise_peptide <- function(batch,batches=c("ZWK","ZYQ","UDP"),verbose=FALSE)
   {
+    if (batch==1) covars <- setdiff(ccovars,pcs) else covars <- ccovars
     code <- batches[batch]
     load(paste0("~/Caprion/pilot/",code,".rda"))
     peptide_ids <- subset(get(paste("mapping",code,sep="_")),Protein==paste0(protein,"_HUMAN"))[["Isotope.Group.ID"]]
     peptide_exprs <- exprs(get(paste("peptide",code,sep="_")))
     peptide_exprs <- subset(peptide_exprs, rownames(peptide_exprs) %in% peptide_ids)
-    peptide_dat <- ids %>%
+    peptide_dat <- dat %>%
                    left_join(data.frame(id=colnames(peptide_exprs),t(peptide_exprs))) %>%
                    filter(!is.na(FID) & grepl(code,id)) %>%
                    select(-id)
-    na_dat <- apply(peptide_dat[setdiff(names(peptide_dat),c("FID","IID"))],1,sum,na.rm=TRUE)
-    if (verbose) write.table(filter(peptide_dat,na_dat!=0),
+    peptides <- setdiff(names(peptide_dat),c("FID","IID","batch",ccovars))
+    mod <- model.matrix(as.formula(paste0("~",paste(covars,collapse="+"))), data=peptide_dat)
+    z <- sapply(names(peptide_dat[peptides]),
+                function(col,verbose=FALSE)
+                {
+                  if (verbose) cat(names(peptide_dat[col]),col,"\n")
+                  y <- gap::invnormal(peptide_dat[[col]])
+                  l <- lm(y~mod[,-1])
+                  r <- y-predict(l,na.action=na.pass)
+                  scale(r)
+                })
+    colnames(z) <- names(peptide_dat[peptides])
+    rownames(z) <- peptide_dat[["IID"]]
+    peptide_lr <- data.frame(peptide_dat[c("FID","IID")],z)
+    names(peptide_lr) <- gsub("^X([0-9])","\\1",names(peptide_lr))
+    na_dat <- apply(peptide_dat[peptides],1,sum,na.rm=TRUE)
+    if (verbose) write.table(filter(peptide_lr,na_dat!=0),
                              file=paste0(analysis,"/peptide/",protein,"/",code,".pheno"),
-                             col.names=gsub("^[X]","",names(peptide_dat)),row.names=FALSE,quote=FALSE)
-    peptide_dat
+                             row.names=FALSE,quote=FALSE)
+    peptide_lr
   }
-  ZWK <- es_peptide(1)
-  ZYQ <- es_peptide(2)
-  UDP <- es_peptide(3)
+  ZWK <- normalise_peptide(1)
+  ZYQ <- normalise_peptide(2)
+  UDP <- normalise_peptide(3)
   caprion <- bind_rows(ZWK,ZYQ,UDP)
   write.table(caprion,file=paste0(analysis,"/peptide/",protein,"/",protein,".pheno"),
-              col.names=gsub("^[X]","",names(caprion)),row.names=FALSE,quote=FALSE)
+              col.names=gsub("^X([0-9])","\\1",names(caprion)),row.names=FALSE,quote=FALSE)
   write.table(caprion,file=paste0(analysis,"/peptide/",protein,"/",protein,".mpheno"),
               col.names=FALSE,row.names=FALSE,quote=FALSE)
 '
@@ -72,11 +88,11 @@ function fastLR()
 {
   export batch=${1}
   export pheno=${analysis}/peptide/${protein}/${protein}.pheno
-  export N=$(awk 'NR==1{print NF-1}' ${pheno})
+  export N=$(awk 'NR==1{print NF-2}' ${pheno})
 
   for col in $(seq ${N})
   do
-  export peptide=$(awk 'NR==1{print $(col+1)}' col=${col} ${pheno})
+  export peptide=$(awk 'NR==1{print $(col+2)}' col=${col} ${pheno})
   gcta-1.9 --mbgen ${pilot}/work/caprion.bgenlist \
            --sample ${pilot}/work/caprion.sample \
            --keep ${pilot}/work/caprion-${batch}.id \

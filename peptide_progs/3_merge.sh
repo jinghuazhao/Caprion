d4673d76e9b6fd5311c98a7f1a5c15f02d22f1d2
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-03-30 21:07:25 +0000

    Deployed 7dcdf1c with MkDocs version: 1.5.3

--- peptide_progs/3_merge.sh
+++ peptide_progs/3_merge.sh
@@ -672,6 +672,11 @@ export signals=${analysis}/work/caprion${suffix}.signals
 export varlist=${analysis}/output/caprion${suffix}.varlist
 
 # all proteins:
+export -f initilise
+export -f step1_pqtl_list
+export -f step2_pqtl_collect
+export -f step3_pqtl_summary
+export -f fplz
 grep -n -f ${analysis}/peptide_progs/benchmark2.names -w  ${varlist} | \
 parallel -C':' '
     export protein_index={1}
@@ -697,23 +702,23 @@ parallel -C':' '
   # 3. Graphical representation
     {
     echo Step 3.
-    export pqtl_peptides=$(sed '1d' ${root}/${protein}.signals | cut -f1 | sort -k1,1n | uniq)
-    export array=$(grep -n -f <(echo ${pqtl_peptides} | tr ' ' '\n') <(echo ${all_peptides} | tr ' ' '\n') | cut -d':' -f1 | tr '\n' ',' | sed 's/.$//')
+    export pqtl_peptides=$(sed "1d" ${root}/${protein}.signals | cut -f1 | sort -k1,1n | uniq)
+    export array=$(grep -n -f <(echo ${pqtl_peptides} | tr " " "\n") <(echo ${all_peptides} | tr " " "\n") | cut -d":" -f1 | tr "\n" "," | sed "s/.$//")
   # 12hr-timeout proteins
   # export i=129, 299 for CO3, ITIH2; 47, 179, 184, 492 for APOB, EPCR, ERAP2, PROC
   # export todo_peptides=$(ls ${analysis}/peptide/CO3/fp/*pdf | xargs -l basename -s -fp.pdf | sort -k1,1 | uniq | grep -f - -v <(sed 's/ /\n/g' <(echo ${pqtl_peptides})))
-  # export array=$(grep -n -f <(echo ${todo_peptides} | tr ' ' '\n') <(echo ${all_peptides} | tr ' ' '\n') | cut -d':' -f1 | tr '\n' ',' | sed 's/.$//')
+  # export array=$(grep -n -f <(echo ${todo_peptides} | tr " " "\n") <(echo ${all_peptides} | tr " " "\n") | cut -d":" -f1 | tr "\n" "," | sed "s/.$//")
     for batch in {1..3}
     do
     (
       head -1 ${root}/${protein}.pheno
-      grep -f <(cut -d' ' -f1 ${pilot}/work/caprion-${batch}.id) ${root}/${protein}.pheno
+      grep -f <(cut -d" " -f1 ${pilot}/work/caprion-${batch}.id) ${root}/${protein}.pheno
     ) > ${root}/work/${protein}-${batch}.pheno
     done
     step3_pqtl_summary
     sbatch ${root}/${protein}-step3.sb
     }
-  # pdfjam ${dir}/*_qqmanhattan.pdf --nup 1x1 --landscape --papersize '{7in,12in}' --outfile ${root}/qq+manhattan.pdf
+  # pdfjam ${dir}/*_qqmanhattan.pdf --nup 1x1 --landscape --papersize "{7in,12in}" --outfile ${root}/qq+manhattan.pdf
   # qpdf --empty --pages $(ls ${dir}/*_qqmanhattan.pdf) -- ${root}/qq+manhattan.pdf
   '
 }

7ea0e5c96617a5cb3c0fdd08ed74b0bde7c336ab
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-06-24 10:21:30 +0100

    Deployed 02858f7 with MkDocs version: 1.5.3

--- docs.sh
+++ docs.sh
@@ -8,14 +8,14 @@ function setup()
   do cp -r -p ../pilot/${f} pilot; done
 }
 
+setup
+mkdocs build
+mkdocs gh-deploy
+
 if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
    module load ceuadmin/libssh/0.10.6-icelake
    module load ceuadmin/openssh/9.7p1-icelake
 fi
-
-setup
-mkdocs build
-mkdocs gh-deploy
 
 git add workflow
 git commit -m "Snakemake profiles"

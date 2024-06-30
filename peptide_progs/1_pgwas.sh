dce6a4294429457c318734d799d78e37999265f2
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-06-30 08:15:59 +0100

    Deployed 516bc27 with MkDocs version: 1.5.3

--- peptide_progs/1_pgwas.sh
+++ peptide_progs/1_pgwas.sh
@@ -172,7 +172,6 @@ export TMPDIR=${HPC_WORK}/work
 export pilot=~/Caprion/pilot
 export analysis=~/Caprion/analysis
 export suffix=_dr
-export signals=${analysis}/work/caprion${suffix}.signals
 export varlist=${analysis}/output/caprion${suffix}.varlist
 
 module load ceuadmin/R

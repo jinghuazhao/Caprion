3fa8bc4fd41ce99ef76f87e21f89aff00ef87943
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-07-04 09:43:46 +0100

    Deployed 0218e65 with MkDocs version: 1.5.3

--- peptide_progs/mz.sh
+++ peptide_progs/mz.sh
@@ -1,29 +1,32 @@
 #!/usr/bin/bash
 
-module load ceuadmin/crux/4.1
 module load ceuadmin/pwiz/3_0_24163_9bfa69a-wine
 module load ceuadmin/wine/8.21
-export sprot=~/rds/public_databases/UniProt/uniprot_sprot.fasta.gz
-export sprot=uniprot_sprot.fasta
-export uniref100=~/rds/public_databases/UniProt/uniref100.fasta.gz
-
+export ZWK=~/Caprion/pre_qc_data/spectral_library_ZWK
 export raw=szwk901104i19901xms1.raw
-# https://crux.ms/fileformats.html
-# module load ceuadmin/pwiz/3_0_24156_80747de not fully functional
-# only works on Windows
 {
-  export ZWK=~/Caprion/pre_qc_data/spectral_library_ZWK
   export mgf=$(echo ${raw} | sed 's/raw/mgf/')
   export mzML=$(echo ${raw} | sed 's/raw/mzML/')
   if [ ! -f ${mgf} ]; then
      echo ${mgf}
      wine64 $(which msconvert.exe) --mgf ${ZWK}/${raw}
-  elif [ ! -f ${mzML} ]; then
+  fi
+  if [ ! -f ${mzML} ]; then
      echo ${mzML}
      wine64 $(which msconvert.exe) --mzML ${ZWK}/${raw}
   fi
 }
+# https://crux.ms/fileformats.html
 
+module load ceuadmin/docker/24.0.5
+docker run -it --rm -e WINEDEBUG=-all -v ${ZWK}:/data chambm/pwiz-skyline-i-agree-to-the-vendor-licenses \
+       wine msconvert /data/szwk901104i19801xms1.raw --filter "peakPicking true 1-"
+# under Ubuntu 22.04 LTS /home/$USER/D/Downloads:/data
+
+module load ceuadmin/crux/4.1
+export sprot=~/rds/public_databases/UniProt/uniprot_sprot.fasta.gz
+export sprot=uniprot_sprot.fasta
+export uniref100=~/rds/public_databases/UniProt/uniref100.fasta.gz
 R --no-save < crux.R
 
 function yeast()

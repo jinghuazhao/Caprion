d124c0257bb3e2be3969cf060762561d0d51fe75
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-09-23 16:54:33 +0100

    Deployed fa574f0 with MkDocs version: 1.5.3

--- docs.sh
+++ docs.sh
@@ -25,8 +25,7 @@ git add README.md pilot/README.md
 git commit -m "README"
 git add pilot/qrcode.svg pilot/qrcode.png
 git commit -m "QR code"
-git add peptide_progs/ 0_compare.sb 0_utils.sb
-git add 1_pca_projection.sh 2_ggm.R 3_wgcna.R 4_pca_clustering.R 5_pgwas.* 6_meta_analysis.* 7_merge* 8_hla.* 9_lookup.* ToDo.sh
+git add misc/ progs/ peptide_progs/
 git commit -m "Analysis"
 git add pilot/caprion.ini pilot/caprion.R pilot/caprion.ipynb
 git commit -m "Primary Code"

cd4e40533fd089504084bf6338236fd779ceb3d8
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-09-26 14:44:52 +0100

    Analysis

--- misc/deCODE_UKB_PPP.sh
+++ misc/deCODE_UKB_PPP.sh
@@ -43,11 +43,15 @@ function caprion()
                dplyr::select(-log10p,-geneChrom,-geneStart,-geneEnd,-SNPChrom,-SNPPos,-cis) %>%
                dplyr::left_join(tbl_dr) %>%
                dplyr::select(Gene,Protein,geneRegion,SNP,Type,EA,OA,EAF,Effect,StdErr,Log10P,Direction,HetISq,logHetP,N,REF,ALT,ALT_FREQS)
+    panel <- pQTLdata::caprion %>%
+             transmute(prot=gsub("_HUMAN","",Protein),uniprot=Accession)
     caprion <- dplyr::mutate(Protein, ProteinSNP=paste0(Protein,"-",SNP),inv_chr_pos_a1_a2(SNP),pos=as.integer(pos)) %>%
-               dplyr::rename(prot=Protein,rsid=SNP,uniprot=Gene)
+               dplyr::rename(prot=Protein,rsid=SNP) %>%
+               dplyr::left_join(panel)
     caprion_dr <- dplyr::mutate(Protein_dr, ProteinSNP=paste0(Protein,"-",SNP),inv_chr_pos_a1_a2(SNP),pos=as.integer(pos)) %>%
-                  dplyr::rename(prot=Protein,rsid=SNP,uniprot=Gene)
-    b <- list()
+                  dplyr::rename(prot=Protein,rsid=SNP) %>%
+                  dplyr::left_join(panel)
+    z <- list()
     for(i in unique(dplyr::pull(caprion,chr)))
     {
        k <- dplyr::filter(caprion,chr %in% i) %>%
@@ -55,15 +59,15 @@ function caprion()
        dr <- dplyr::filter(caprion_dr,chr %in% i) %>%
              dplyr::select(chr,pos,uniprot,rsid,prot)
        bfile <- file.path(INF,"INTERVAL","per_chr",paste0("snpid",i))
-       b[[i]] <- pQTLtools::novelty_check(k,dr,ldops=list(bfile=bfile,plink=plink))
+       z[[i]] <- pQTLtools::novelty_check(k,dr,ldops=list(bfile=bfile,plink=plink))
     }
-    b[["23"]] <- dplyr::mutate(b[["X"]],known.seqnames="23",query.seqnames="23")
-    r <- dplyr::filter(dplyr::bind_rows(b[-which(names(b)=="X")]),r2>=0.8) %>%
+    z[["23"]] <- dplyr::mutate(z[["X"]],known.seqnames="23",query.seqnames="23")
+    r <- dplyr::filter(dplyr::bind_rows(z[-which(names(z)=="X")]),r2>=0.8) %>%
          dplyr::rename(known.gene=known.uniprot,query.gene=query.uniprot)
     s <- dplyr::mutate(r,seqnames=as.integer(known.seqnames),pos=as.integer(known.pos)) %>%
          dplyr::arrange(seqnames,pos) %>%
          dplyr::select(-known.start,-known.end,-query.seqnames,-query.start,-query.end,-seqnames,-pos)
-    save(caprion,caprion_dr,r,s,file=file.path(analysis,"reports","caprion-caprion_dr.rda"))
+    save(panel,caprion,caprion_dr,r,s,file=file.path(analysis,"reports","caprion-caprion_dr.rda"))
     cvt <- "~/Caprion/analysis/work/caprion_dr.cis.vs.trans"
     f <- read.csv(cvt) %>%
          mutate(seqname=paste0("chr",SNPChrom),start=as.integer(SNPPos),end=SNPPos) %>%
@@ -73,11 +77,13 @@ END
 
 function deCODE()
 {
-  export v4=SomaLogicv4.tsv
   R --no-save -q <<END
     options(width=200)
     suppressMessages(library(dplyr))
     suppressMessages(require(openxlsx))
+    suppressMessages(library(rtracklayer))
+    suppressMessages(library(pQTLtools))
+    suppressMessages(require(GenomicRanges))
     analysis <- Sys.getenv("analysis")
     INF <- Sys.getenv("INF")
     plink <- Sys.getenv("plink")
@@ -86,43 +92,47 @@ function deCODE()
     # deCODE
     xlsx <- file.path(Sys.getenv("deCODE"),"doc","ferkingstad21.xlsx")
     SomaLogicv4 <- openxlsx::read.xlsx(xlsx,sheet=1,startRow=3,colNames=TRUE,cols=1:12)
-    out <- file.path(Sys.getenv("analysis"),"deCODE",Sys.getenv("v4"))
-    write.table(SomaLogicv4,file=out,quote=FALSE,row.names=FALSE,sep="\t")
     sentinels <- openxlsx::read.xlsx(xlsx,sheet=2,startRow=3,colNames=TRUE) %>%
-               select(10,9,3,7,11,14,15,18,19,27,29) %>%
+               dplyr::select(10,9,3,7,11,14,15,18,19,27,29) %>%
                setNames(c("SeqId","uniprot","gene","prot","variant","chr","pos","A1","A2","beta","log10p")) %>%
-               mutate(chr=gsub("chr","",chr),snpid=gap::chr_pos_a1_a2(chr,pos,A1,A2,prefix=""),
-                      se=TwoSampleMR::get_se(beta,10^(-log10p)))
+               dplyr::mutate(chr=gsub("chr","",chr),snpid=gap::chr_pos_a1_a2(chr,pos,A1,A2,prefix=""),
+                             se=TwoSampleMR::get_se(beta,10^(-log10p)))
     rep38 <- sentinels %>%
-             mutate(seqname=chr,start=as.integer(pos),end=pos) %>%
-                    arrange(chr,pos)
+             dplyr::mutate(seqname=chr,start=as.integer(pos),end=pos) %>%
+             dplyr::arrange(chr,pos)
     gr <- with(rep38,GenomicRanges::GRanges(seqnames=seqname,
                                     IRanges::IRanges(start,end,names=snpid),
                                     rsid=variant,chr38=chr,pos38=pos,prot=prot,uniprot=uniprot,
                                     beta=beta,se=se,log10p=log10p))
-    suppressMessages(library(rtracklayer))
-    path <- system.file(package="liftOver", "extdata", "hg38ToHg19.over.chain")
-    ch <- rtracklayer::import.chain(path)
+    f <- system.file(package="liftOver", "extdata", "hg38ToHg19.over.chain")
+    ch <- rtracklayer::import.chain(f)
     GenomeInfoDb::seqlevelsStyle(gr) <- "UCSC"
-    gr19 <- rtracklayer::liftOver(gr,ch) %>%
-            data.frame %>%
-            dplyr::mutate(seqnames=gsub("chr","",seqnames)) %>%
-            dplyr::select(-group,-end,-width,-strand) %>%
-            dplyr::rename(snpid=group_name,chr=seqnames,pos=start)
-    write.table(gr19,file=file.path(analysis,"deCODE","deCODE.tsv"),row.names=FALSE,quote=FALSE,sep="\t")
-    suppressMessages(library(pQTLtools))
-    b <- list()
+    deCODE <- rtracklayer::liftOver(gr,ch) %>%
+              data.frame %>%
+              dplyr::mutate(seqnames=gsub("chr","",seqnames)) %>%
+              dplyr::select(-group,-end,-width,-strand) %>%
+              dplyr::rename(snpid=group_name,chr=seqnames,pos=start)
+    z <- list()
     for(i in unique(dplyr::pull(caprion_dr,chr)))
     {
        dr <- dplyr::filter(caprion_dr,chr %in% i) %>%
             dplyr::select(chr,pos,uniprot,rsid,prot)
-       decode <- dplyr::filter(gr19,chr %in% i) %>%
+       decode <- dplyr::filter(deCODE,chr %in% i) %>%
                  dplyr::select(chr,pos,uniprot,snpid,prot) %>%
                  dplyr::rename(rsid=snpid)
        bfile <- file.path(INF,"INTERVAL","per_chr",paste0("snpid",i))
-       b[[i]] <- pQTLtools::novelty_check(decode,dr,ldops=list(bfile=bfile,plink=plink))
+       z[[i]] <- pQTLtools::novelty_check(decode,dr,ldops=list(bfile=bfile,plink=plink))
     }
-    b[["23"]] <- dplyr::mutate(b[["X"]],known.seqnames="23",query.seqnames="23")
+    z[["23"]] <- dplyr::mutate(z[["X"]],known.seqnames="23",query.seqnames="23")
+    u <- dplyr::filter(dplyr::bind_rows(z[-which(names(z)=="X")]),r2>=0.8) %>%
+         dplyr::rename(known.gene=known.uniprot,query.gene=query.uniprot)
+    v <- dplyr::mutate(u,seqnames=as.integer(known.seqnames),pos=as.integer(known.pos)) %>%
+         dplyr::arrange(seqnames,pos) %>%
+         dplyr::select(-known.start,-known.end,-query.seqnames,-query.start,-query.end,-seqnames,-pos)
+    save(deCODE,u,v,file=file.path(analysis,"deCODE","deCODE.rda"))
+    f <- file.path(Sys.getenv("analysis"),"deCODE","SomaLogicv4.tsv")
+    write.table(SomaLogicv4,file=f,quote=FALSE,row.names=FALSE,sep="\t")
+    write.table(gr19,file=file.path(analysis,"deCODE","deCODE.tsv"),row.names=FALSE,quote=FALSE,sep="\t")
 END
 }
 
@@ -133,6 +143,7 @@ function UKB_PPP()
     suppressMessages(require(dplyr))
     suppressMessages(require(openxlsx))
     suppressMessages(require(pQTLtools))
+    suppressMessages(require(GenomicRanges))
     analysis <- Sys.getenv("analysis")
     INF <- Sys.getenv("INF")
     plink <- Sys.getenv("plink")
@@ -141,34 +152,39 @@ function UKB_PPP()
     # UKB_PPP list
     results <- "/rds/project/jmmh2/rds-jmmh2-results/public/proteomics"
     url <- file.path(results,"UKB-PPP","doc","sun23.xlsx")
-    ST10 <- read.xlsx(url,"ST10",startRow=3) %>%
+    ST10 <- read.xlsx(url,"ST10",startRow=4) %>%
             dplyr::mutate(uniprot=Target.UniProt,rsid=rsID,prot=Assay.Target) %>%
             dplyr::mutate(prot_rsid=paste0(uniprot,"-",rsid))
-    caprion <- c(with(pQTLdata::caprion,uniprot),with(caprion_dr,uniprot)) %>%
-               unique()
-    overlap <- dplyr::filter(ST10,uniprot %in% caprion)
+    uniprots <- c(with(panel,uniprot),with(caprion_dr,uniprot)) %>%
+                unique()
+    overlap <- dplyr::filter(ST10,uniprot %in% uniprots)
     dim(overlap)
     UKB_PPP <- dplyr::mutate(overlap,
                chrpos=strsplit(overlap[["Variant.ID.(CHROM:GENPOS.(hg37):A0:A1:imp:v1)"]],":"),
-               chr=as.integer(unlist(lapply(chrpos,"[[",1))),
-               pos=as.integer(unlist(lapply(chrpos,"[[",2))),
-               chrpos=paste(chr,pos,sep=":"))
-    suppressMessages(require(GenomicRanges))
-    replication <- read.delim(file.path(find.package("pQTLtools"),"tests","UKB-PPP.txt")) %>%
-                   dplyr::select(known.seqnames,known.rsid,query.rsid,query.prot)
-    variant_list <- unique(c(dplyr::pull(replication,known.rsid),
-                             dplyr::pull(replication,query.rsid)))
-    b <- list()
-    for(i in unique(dplyr::pull(caprion_dr,Chromosome)))
+               chr=unlist(lapply(chrpos,"[[",1)),
+               pos=unlist(lapply(chrpos,"[[",2)),
+               a1=unlist(lapply(chrpos,"[[",3)),
+               a2=unlist(lapply(chrpos,"[[",4))) %>%
+               dplyr::filter(!is.na(a1)&!is.na(a2)) %>%
+               dplyr::mutate(rsid=gap::chr_pos_a1_a2(chr,pos,a1,a2)) %>%
+               dplyr::select(-chrpos)
+    z <- list()
+    for(i in unique(dplyr::pull(caprion_dr,chr)))
     {
        u <- dplyr::filter(UKB_PPP,chr %in% i) %>%
             dplyr::select(chr,pos,uniprot,rsid,prot)
-       m <- dplyr::filter(caprion,Chromosome %in% i) %>%
+       m <- dplyr::filter(caprion_dr,chr %in% i) %>%
             dplyr::select(chr,pos,uniprot,rsid,prot)
        bfile <- file.path(INF,"INTERVAL","per_chr",paste0("snpid",i))
-       b[[i]] <- novelty_check(u,m,ldops=list(bfile=bfile,plink=plink))
+       z[[i]] <- novelty_check(u,m,ldops=list(bfile=bfile,plink=plink))
     }
-    replication2 <- dplyr::filter(bind_rows(b), r2>=0.8)
+    z[["23"]] <- dplyr::mutate(z[["X"]],known.seqnames="23",query.seqnames="23")
+    w <- dplyr::filter(dplyr::bind_rows(z[-which(names(z)=="X")]),r2>=0.8) %>%
+         dplyr::rename(known.gene=known.uniprot,query.gene=query.uniprot)
+    x <- dplyr::mutate(w,seqnames=as.integer(known.seqnames),pos=as.integer(known.pos)) %>%
+         dplyr::arrange(seqnames,pos) %>%
+         dplyr::select(-known.start,-known.end,-query.seqnames,-query.start,-query.end,-seqnames,-pos)
+    save(UKB_PPP,w,x,file=file.path(analysis,"deCODE","deCODE.rda"))
     prot_rsid <- with(novel_data %>%
                  dplyr::left_join(gap.datasets::inf1[c("prot","gene")]),paste0(gene,"-",rsid))
     prot_rsid_repl <- with(replication2,paste0(query.prot,"-",query.rsid))

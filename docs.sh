2bf612c8a32334b9bd1d2b2b72c8ed6c7f23f833
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-09-29 14:12:02 +0100

    Deployed 088a2b0 with MkDocs version: 1.5.3

---
+++ docs.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+function setup()
+{
+  module load python/3.8
+  source ~/rds/public_databases/software/py38/bin/activate
+}
+
+if [ "$(uname -n | sed 's/-[0-9]*$//')" == "login-q" ]; then
+   module load ceuadmin/libssh/0.10.6-icelake
+   module load ceuadmin/openssh/9.7p1-icelake
+fi
+
+setup
+mkdocs build
+mkdocs gh-deploy
+
+git add workflow
+git commit -m "Snakemake profiles"
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add docs/
+git commit -m "docs"
+git add pilot/
+git commit -m "Pilot studies"
+git add misc/ progs/ peptide_progs/
+git commit -m "Analysis"
+git add mkdocs.yml
+git commit -m "mkdocs.yml"
+git push
+# git remote set-url origin https://github.com/jinghuazhao/Caprion.git

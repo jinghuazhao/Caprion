f1fc65cd5a5e973f78412949a8d8b310e4b22cd2
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-11-15 17:47:29 +0000

    Deployed f01db15 with MkDocs version: 1.5.3

--- misc/dup-extract.sh
+++ misc/dup-extract.sh
@@ -1,29 +1,22 @@
 #!/usr/bin/bash
 
-function initialise()
-{
-  for d in sentinels/slurm means work qqmanhattanlz vep
-  do
+# Define the root path (assuming it was set earlier)
+export root=${root:-/home/jhz22/Caprion/analysis/dup}
+
+# Initialize directories
+function initialise() {
+  for d in sentinels/slurm means work qqmanhattanlz vep; do
     if [ ! -d ${root}/${d} ]; then mkdir -p ${root}/${d}; fi
   done
 }
 
-function step1_pqtl_list()
-{
-cat <<'EOL'> ${root}/dup-step1.sb
+# Step 1: Create the task list
+function step1_pqtl_list() {
+  cat <<'EOL' > ${root}/dup-step1.sh
 #!/usr/bin/bash
 
-#SBATCH --job-name=_dup1
-#SBATCH --mem=28800
-#SBATCH --time=12:00:00
-
-#SBATCH --account PETERS-SL3-CPU
-#SBATCH --partition icelake-himem
-
-#SBATCH --export ALL
-#SBATCH --array=1-_N_
-#SBATCH --output=ROOT/sentinels/slurm/_step1_%A_%a.o
-#SBATCH --error=ROOT/sentinels/slurm/_step1_%A_%a.e
+export root=${root}
+export isotope=${1}  # Take isotope as an argument
 
 . /etc/profile.d/modules.sh
 module purge
@@ -33,12 +26,10 @@ module load samtools/1.13/gcc/zwxn7ug3
 module load libiconv/1.16/intel/64iicvbf
 
 export TMPDIR=${HPC_WORK}/work
-export isotope=$(head -1 ${root}/ZWK.pheno | awk -vn=${SLURM_ARRAY_TASK_ID} '{print $(n+2)}')
 
-function pgz()
-# 1. extract all significant SNPs
-{
-  cat <(zcat ${root}/ZWK-1-${isotope}-fastGWA.gz | awk 'NR>1 && $7>=0.001 && $7 <= 0.999 && $10<=5e-8')
+function pgz() {
+  # 1. extract all significant SNPs
+  cat <(zcat ${root}/ZWK-1-${isotope}.fastGWA.gz | awk 'NR>1 && $7>=0.001 && $7 <= 0.999 && $10<=5e-8') \
       <(zcat ${root}/ZWK-1-${isotope}-chrX.fastGWA.gz | awk 'NR>1 && $7>=0.001 && $7 <= 0.999 && $10<=5e-8') | \
   sort -k1,1n -k3,3n | \
   gzip -f > ${root}/sentinels/${isotope}.p.gz
@@ -48,27 +39,28 @@ function _HLA()
 # 2. handling HLA
 {
   (
-    zcat ${root}/METAL/${isotope}-1.tbl.gz | awk -vOFS="\t" 'NR==1{$1="Chrom";$2="Start" "\t" "End";print}'
+    zcat ${root}/ZWK-1-${isotope}.fastGWA.gz | awk -vOFS="\t" 'NR==1{$1="Chrom";$2="Start" "\t" "End";$3="SNP";print}'
     zcat ${root}/sentinels/${isotope}.p.gz | \
-    awk -vOFS="\t" '{$1="chr" $1; start=$2-1;$2=start "\t" $2;print}' | \
-    awk '!($1 == "chr6" && $3 >= 25392021 && $3 < 33392022)'
+    awk -vOFS="\t" '{$1="chr"$1; snp=$2; $2=$3"\t"$3;$3=snp;print}' | \
+    awk '!($1 == "chr6" && $2 >= 25392021 && $2 < 33392022)'
     zcat ${root}/sentinels/${isotope}.p.gz | \
-    awk -vOFS="\t" '{$1="chr" $1; start=$2-1;$2=start "\t" $2;print}' | \
-    awk '$1 == "chr6" && $3 >= 25392021 && $3 < 33392022' | \
-      sort -k13,13g | \
+    awk -vOFS="\t" '{$1="chr"$1; snp=$2; $2=$3"\t"$3;$3=snp;print}' | \
+    awk '$1 == "chr6" && $2 >= 25392021 && $2 < 33392022' | \
+      sort -k11,11g | \
       awk 'NR==1'
-  ) > ${root}/sentinels/${isotope}_nold.p
-  export lines=$(wc -l ${root}/sentinels/${isotope}_nold.p | cut -d' ' -f1)
+  ) > ${root}/sentinels/${isotope}.nold
+  export lines=$(wc -l ${root}/sentinels/${isotope}.nold | cut -d' ' -f1)
   if [ $lines -eq 1 ]; then
-     echo removing ${isotope}_nold with $lines lines
-     rm ${root}/sentinels/${isotope}_nold.p
+     echo removing ${isotope}.nold with $lines lines
+     rm ${root}/sentinels/${isotope}.nold
   fi
 }
 
-function sentinels()
-{
+function sentinels() {
+  # 3. merge and filter significant SNPs
+  if [ -f ${root}/sentinels/${isotope}.nold ]; then
   (
-    mergeBed -i ${root}/sentinels/${isotope}_nold.p -d 1000000 -c 13 -o min | \
+    mergeBed -i ${root}/sentinels/${isotope}.nold -d 1000000 -c 11 -o min | \
     awk -v OFS="\t" -v trait=${isotope} '
     {
       if(NR==1) print "Chrom", "Start", "End", "P", "trait"
@@ -76,15 +68,14 @@ function sentinels()
     }'
   ) > ${root}/sentinels/${isotope}.merged
   (
-    cut -f1-4,13 ${root}/sentinels/${isotope}_nold.p| \
+    cut -f1-4,11 ${root}/sentinels/${isotope}.nold | \
     bedtools intersect -a ${root}/sentinels/${isotope}.merged -b - -wa -wb | \
     awk '$4==$10' | \
-    cut -f1-5,9,10 | \
     awk -v OFS="\t" -v trait=${isotope} '
     {
       if(NR==1) print "Chrom", "Start", "End", "P", "trait", "MarkerName", "CHR", "POS", "SNP", "P_check"
-      chr=gsub(/chr/,"",$1)
-      print $1,$2,$3,$4,trait,$1":"$3,chr,$3,$6,$7
+      chr=gsub(/chr/,"",$6)
+      print $1,$2,$3,$4,$5,$6":"$7,chr,$7,$9,$10
     }'
   ) | uniq > ${root}/sentinels/${isotope}.sentinels
 
@@ -107,16 +98,27 @@ function sentinels()
     if (exists(signal_file)) unlink(signal_file)
     write.table(m[,cols],file=signal_file,row.names=FALSE,quote=FALSE,sep="\t")
   '
+  fi
 }
 
+# Now we run the functions
 for cmd in pgz _HLA sentinels; do $cmd; done
 EOL
 
-export N=$(head -1 ${root}/ZWK.pheno | awk '{print NF-2}')
-sed -i "s|ROOT|${root}|;s|_N_|${N}|" ${root}/dup-step1.sb
+  export N=$(head -1 ${root}/ZWK.pheno | awk '{print NF-2}')
+  sed -i "s|ROOT|${root}|;s|_N_|${N}|" ${root}/dup-step1.sh
 }
+
+# Main script execution
 export analysis=~/Caprion/analysis
 export root=${analysis}/dup
 initialise
 step1_pqtl_list
-sbatch ${root}/dup-step1.sb
+
+# Now create the parallel execution environment:
+
+# Extract the list of isotopes (one for each task), and run in parallel
+isotopes=$(head -1 ${root}/ZWK.pheno | awk '{for(i=3;i<=NF;i++) print $i}')
+
+# Use GNU Parallel to run the script in parallel
+echo "$isotopes" | parallel -j 10 --bar ${root}/dup-step1.sh

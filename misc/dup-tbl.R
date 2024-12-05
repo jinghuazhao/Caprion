8a5fb37c840848a2ce7625c379e98170d648d2d8
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2024-12-05 18:49:32 +0000

    Analysis

--- misc/dup-tbl.R
+++ misc/dup-tbl.R
@@ -8,5 +8,5 @@ m <- read.delim(file.path(analysis,"dup","dup.merge"))
 s <- read.delim(file.path(analysis,"dup","dup.signals")) %>%
      dplyr::select(trait,P,SNP)
 signals <- left_join(m,s)
-tbl <- dplyr::right_join(lst,signals,by=c("Isotope.Group.ID"="trait"))
+tbl <- dplyr::left_join(lst,signals,by=c("Isotope.Group.ID"="trait"))
 write.table(tbl, file = file.path(analysis,"dup","dup.tbl"), sep = "\t", quote = FALSE, row.names = FALSE, col.names = TRUE)
